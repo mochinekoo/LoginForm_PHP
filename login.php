6aeca5a03799c787fa7cd824c3fcf581faaa260a
mochinekoo <mochinekoo0113@gmail,com>
2026-06-07 15:47:00 +0900

    [add] ログインすることができるようになりました

--- login.php
+++ login.php
@@ -1,20 +1,22 @@
 <?php
     $name = $_POST['username'];
     $password = $_POST['password'];
-    $dsn = 'mysql:host=localhost; dbname=testdb; charset=utf8';
+    $dsn = 'mysql:host=localhost; dbname=test; charset=utf8';
 
     try {
-        $pdo = new PDO($dsn, 'user', '0113');
+        $pdo = new PDO($dsn, 'user', 'userpass1');
     } catch (PDOException $e) {
         echo '接続できませんでした';
     }
 
-    $sql = "SELECT * FROM users WHERE name = :name AND password = :password";
+    $sql = "SELECT * FROM users WHERE user_name = :name AND user_pass = :password";
     $stmt = $pdo->prepare($sql);
+    $stmt->execute([':name' => $name, ':password' => $password]);
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
     if ($row) {
         echo 'ログイン成功';
     } else {
-        echo 'ログイン失敗';
+        echo 'ユーザー名かパスワードが違います';
     }
 ?>
